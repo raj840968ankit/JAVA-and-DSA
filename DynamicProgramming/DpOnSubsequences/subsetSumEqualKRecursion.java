ee56651a53d4ac4caf3cd3f545a61774c0ac6eca
Ankit Raj <rajankit840968@gmail.com>
2025-03-24 21:27:57 +0530

    Add recursive solution for subset sum equal to K problem

---
+++ DynamicProgramming/DpOnSubsequences/subsetSumEqualKRecursion.java
@@ -0,0 +1,23 @@
+package DynamicProgramming.DpOnSubsequences;
+
+public class subsetSumEqualKRecursion {
+    public static boolean existSum(int i, int sum, int[] arr){
+        if(i < 0) return false;
+        if(sum == 0) return true;
+        if(sum >= arr[i]){
+            if(existSum(i - 1, sum - arr[i], arr)){
+                return true;
+            }
+        }
+        if(existSum(i - 1, sum, arr)){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] arr ={1, 2, 3};
+        int sum = 6;
+        boolean res = existSum(arr.length - 1, sum, arr);
+        System.out.println(res);
+    }
+}
