eb90495a3379ac292f2c63d8ec45a44e1c1c3e96
Ankit Raj <rajankit840968@gmail.com>
2025-04-04 21:36:00 +0530

    Add solution for pangram check in Java

---
+++ StringsCodeWithMik/stringPangram2.java
@@ -0,0 +1,21 @@
+package StringsCodeWithMik;
+
+public class stringPangram2 {
+    //Question states that "a pangram is a sentence that contains every letter of alphabet at least once"
+    public static void main(String[] args) {
+        String sentence = "thequickbrownfoxjumpsoverthelazydog";
+        boolean ans = true;
+        int[] letter = new int[26];  //hash array that will contain alphabet frequency
+        for(int i = 0; i < sentence.length(); i++){
+            int position = sentence.charAt(i) - 'a';
+            letter[position] += 1;
+        }
+        for(int i = 0; i < letter.length; i++){
+            if(letter[i] == 0){
+                ans = false;
+            }
+        }
+        System.out.println(ans);
+    }
+}
+
